c58dce689e341a8dd5029dae70d030c46055b283
easakibahmad <sakib.sgc.du@gmail.com>
2024-10-02 16:49:39 +0600

    feat: update kmp ui

--- src/components/KMPAlgo/KMPAlgo.tsx
+++ src/components/KMPAlgo/KMPAlgo.tsx
@@ -1,17 +1,57 @@
 import React, { useState } from "react";
-
 import { kmpSearch } from "../../utils/KMPAlgo/kmp";
-
 import "./KMPAlgo.css";
 
 const KMPAlgo: React.FC = () => {
   const [text, setText] = useState<string>("");
   const [pattern, setPattern] = useState<string>("");
   const [results, setResults] = useState<number[]>([]);
+  const [highlightedText, setHighlightedText] = useState<JSX.Element | null>(
+    null
+  );
 
   const handleSearch = () => {
     const foundIndexes = kmpSearch(text, pattern);
     setResults(foundIndexes);
+    setHighlightedText(highlightMatches(text, pattern, foundIndexes)); // Update highlighted text here
+  };
+
+  // Function to highlight matches in the text
+  const highlightMatches = (
+    text: string,
+    pattern: string,
+    indices: number[]
+  ) => {
+    if (indices.length === 0) return null; // Return null if no matches
+
+    const parts: JSX.Element[] = [];
+    let lastIndex = 0;
+
+    indices.forEach((index) => {
+      if (lastIndex < index) {
+        parts.push(
+          <span key={`text-${lastIndex}`}>
+            {text.substring(lastIndex, index)}
+          </span>
+        );
+      }
+
+      parts.push(
+        <span key={`match-${index}`} className="kmp__highlight">
+          {text.substring(index, index + pattern.length)}
+        </span>
+      );
+
+      lastIndex = index + pattern.length;
+    });
+
+    if (lastIndex < text.length) {
+      parts.push(
+        <span key={`text-${lastIndex}`}>{text.substring(lastIndex)}</span>
+      );
+    }
+
+    return <span>{parts}</span>; // Return the parts wrapped in a span
   };
 
   return (
@@ -36,6 +76,7 @@ const KMPAlgo: React.FC = () => {
         ) : (
           <h2>No matches found</h2>
         )}
+        <div className="kmp__highlighted-text">{highlightedText}</div>
       </div>
     </div>
   );
